e8ce55dfd42de68923e529f50c74fc94bea92176
debrsa01 <debrsa01@luther.edu>
2017-11-21 14:18:54 -0600

    ex9 almost done, finished c2f

--- src/exercise9/Ex9Converter.java
+++ src/exercise9/Ex9Converter.java
@@ -5,27 +5,27 @@ package exercise9;
  * @author yasiro01
  */
 public class Ex9Converter {
-  public Double c2f(Double valueFrom) {
-    throw new UnsupportedOperationException();
+  public static Double c2f(Double valueFrom) {
+    return (valueFrom * 9/5) + 32;
   }
 
   public Double c2k(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return valueFrom + 273.15;
   }
 
   public Double f2c(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return (valueFrom - 32) * 5/9;
   }
 
   public Double f2k(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return (valueFrom - 32) * 5/9 + 273.15;
   }
 
   public Double k2c(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return valueFrom - 273.15;
   }
 
   public Double k2f(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return (valueFrom - 273.15) * 9/5 + 32;
   }
 }
