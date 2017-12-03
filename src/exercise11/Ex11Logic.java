6550ac56d75d870a60524d23c54e8f85a0bed9e8
debrsa01 <debrsa01@luther.edu>
2017-12-02 23:09:10 -0600

    no idea how to make findMinItem work

--- src/exercise11/Ex11Logic.java
+++ src/exercise11/Ex11Logic.java
@@ -1,6 +1,8 @@
 package exercise11;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Exercise 11 logic
@@ -15,6 +17,9 @@ public class Ex11Logic {
    * @return length of the parameter's string representation
    */
   // TODO: Implement method strLen
+    public <T> int strLen(T item){
+        return item.toString().length();
+    }
   
   /**
    * Task 2
@@ -25,7 +30,18 @@ public class Ex11Logic {
    * @return the greater of three parameters
    */
   // TODO: Implement method greater
+    public <T extends Comparable> T greater (T item1, T item2, T item3){
+        T greatest = item1;
+        if(greatest.compareTo(item2) < 0){
+            greatest = item2;
+        }
+        if(greatest.compareTo(item3) < 0){
+            greatest = item3;
+        }
+        return greatest;
+    }
   
+    
   /**
    * Task 3
    * @param <T>
@@ -33,6 +49,13 @@ public class Ex11Logic {
    * print a list of items
    */
   // TODO: Implement method printList
+    
+    public <T> void printList(T ... list){
+        for (T item : list){
+            System.out.print(item);
+            System.out.print(" ");
+        }
+    }
   
   /**
    * Task 4
@@ -41,6 +64,23 @@ public class Ex11Logic {
    * @return the smallest item in the collection
    */
   // TODO: Implement method findMinItem
+
+    public <T extends Comparable> T findMinItem(T ... list) {
+        T min = null;
+        for (T item : list){
+//            System.out.println(item);
+            if(min == null){
+                min = item;
+            }
+            if(item.compareTo(min) < 0){
+                min = item;
+            }
+        }
+        
+        return min;
+    }
+
+    
 }
 
   /**
@@ -51,3 +91,37 @@ public class Ex11Logic {
    * Implement the constructor, getters, setters, and toString
    */
   // TODO: Implement class Foo
+    
+
+class Foo<T, U> {
+  private T fuzz;
+  private U buzz;
+  
+  public Foo(T fuzz, U buzz) {
+    this.fuzz = fuzz;
+    this.buzz = buzz;
+  }
+
+    public T getFuzz() {
+        return fuzz;
+    }
+
+    public void setFuzz(T fuzz) {
+        this.fuzz = fuzz;
+    }
+
+    public U getBuzz() {
+        return buzz;
+    }
+
+    public void setBuzz(U buzz) {
+        this.buzz = buzz;
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" + "fuzz=" + fuzz + ", buzz=" + buzz + '}';
+    }
+  
+    
+}
